c550e64887fb11f1b537efa8ecd4df324b9339f3
VasyaPikh <pikhvasya@gmail.com>
2021-10-22 19:33:15 +0000

    add time

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 function send() {
   let x = document.getElementById("myText")
   document.getElementById("demo").innerHTML = x.value
-  x.value = ""
+  x.value = "";
 }
-
-function setData () {
-  let now = new Date().toLocaleString();
-  const click = document.getElementById('time-new');
-  }
